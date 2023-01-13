d45b28195dcf90f61b1adb0780b4d39c73a42987
Hijjaj <hijjaj.refza.ft20@mail.umy.ac.id>
2023-01-13 13:43:03 +0700

    menambahkan Surat.java dan SuratJpaController.java

---
+++ src/main/java/com/example/FINAL_EXAM/Surat.java
@@ -0,0 +1,133 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.FINAL_EXAM;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author LENOVO
+ */
+@Entity
+@Table(name = "surat")
+@NamedQueries({
+    @NamedQuery(name = "Surat.findAll", query = "SELECT s FROM Surat s"),
+    @NamedQuery(name = "Surat.findById", query = "SELECT s FROM Surat s WHERE s.id = :id"),
+    @NamedQuery(name = "Surat.findByNoSurat", query = "SELECT s FROM Surat s WHERE s.noSurat = :noSurat"),
+    @NamedQuery(name = "Surat.findByJudul", query = "SELECT s FROM Surat s WHERE s.judul = :judul"),
+    @NamedQuery(name = "Surat.findByTembusan", query = "SELECT s FROM Surat s WHERE s.tembusan = :tembusan"),
+    @NamedQuery(name = "Surat.findByTs", query = "SELECT s FROM Surat s WHERE s.ts = :ts")})
+public class Surat implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Column(name = "NO_SURAT")
+    private Integer noSurat;
+    @Column(name = "JUDUL")
+    private String judul;
+    @Column(name = "TEMBUSAN")
+    private String tembusan;
+    @Lob
+    @Column(name = "FILE")
+    private byte[] file;
+    @Column(name = "TS")
+    @Temporal(TemporalType.DATE)
+    private Date ts;
+
+    public Surat() {
+    }
+
+    public Surat(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNoSurat() {
+        return noSurat;
+    }
+
+    public void setNoSurat(Integer noSurat) {
+        this.noSurat = noSurat;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public String getTembusan() {
+        return tembusan;
+    }
+
+    public void setTembusan(String tembusan) {
+        this.tembusan = tembusan;
+    }
+
+    public byte[] getFile() {
+        return file;
+    }
+
+    public void setFile(byte[] file) {
+        this.file = file;
+    }
+
+    public Date getTs() {
+        return ts;
+    }
+
+    public void setTs(Date ts) {
+        this.ts = ts;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Surat)) {
+            return false;
+        }
+        Surat other = (Surat) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.example.FINAL_EXAM.Surat[ id=" + id + " ]";
+    }
+    
+}
